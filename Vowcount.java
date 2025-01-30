8ba01e99294ec5e501b9b5533b14a2e631e77790
AnushaPadii <anushapadi142@gmail.com>
2025-01-30 22:13:17 +0530

    Add files via upload

---
+++ Vowcount.java
@@ -0,0 +1,12 @@
+public class Vowcount {
+    public static void main(String[] args) {
+        String str = "Hii, Dear";
+        int count = 0;
+        for (char ch : str.toLowerCase().toCharArray()) {
+            if ("aeiou".indexOf(ch) != -1) {
+                count++;
+            }
+        }
+        System.out.println("Number of vowels: " + count);
+    }
+}
